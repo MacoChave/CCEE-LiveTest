692b30805af5a82bdc129998da90244809b2bc70
Maco Chave <catalantt@gmail.com>
2020-02-06 21:26:16 -0600

    Deploy app on firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD9Vt3SVDSi6LXwlBru1EhU8EnMwj73ZGY",
+    authDomain: "ccee-test.firebaseapp.com",
+    databaseURL: "https://ccee-test.firebaseio.com",
+    projectId: "ccee-test",
+    storageBucket: "ccee-test.appspot.com",
+    messagingSenderId: "1036420543830",
+    appId: "1:1036420543830:web:29e3ebbae457fc3f00d2f7",
+    measurementId: "G-721MZ44SJS"
+  }
 };
